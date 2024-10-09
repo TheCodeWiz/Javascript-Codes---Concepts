30b6770977a987de2a81b96bb47cf313cb4ffd4d
TheCodeWiz <manavbhatt1409@gmail.com>
2024-10-10 00:31:06 +0530

    different datatypes and their usage

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,31 @@
+let name = "manav"
+let age = 19
+let isLoggedIn = true
+
+/*
+                This are all primitives datatypes
+    
+   datatype    range                    usage
+1. number      2 to the power of 53           - 
+2. bigint      larger numbers           used in trading apps , stock market, reddit apps, etc.
+3. string           -                   written in single or double quotes
+4. boolean     true / false             used of confirmations , responses
+5. null        it is a standalone       it is a representation of no value / empty value
+               value in js
+6. undefined       -                    it is displayed when value is not defined and not like null
+7. symbol          -                    used during coding in react for showing uniqueness
+*/
+
+/*
+                This is all about objects in js
+
+
+*/
+
+console.log(typeof undefined);//o/p is undefined only
+console.log(typeof null);//o/p is object only
+
+// type of null is object 
+// type of undefined is undefined 
+
+console.log(typeof NaN);//o/p is number 
